22a440a91da1c4fe8dddf43a96a6931f25b1bfd3
ahhutt <olehlazaruk0@gmail.com>
2026-09-17 17:01:29 +0300

    /

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("A\tB\tr\tnotr\tp -> q\tr -> p")
+	fmt.Println("A\tB\tr\tnotr\tp -> q\tr -> p\tF1")
 	fmt.Println("-----------------------------------------------")
 
 	bools := []bool{true, false}
@@ -16,8 +16,9 @@ func main() {
 				implication1 := !p || q
 				implication2 := !r || p
 				notR := !r
+				f1 := implication1 == implication2
 
-				fmt.Printf("%t\t%t\t%t\t%t\t%t\t%t\n", p, q, r, notR, implication1, implication2)
+				fmt.Printf("%t\t%t\t%t\t%t\t%t\t%t\t%t\n", p, q, r, notR, implication1, implication2, f1)
 			}
 		}
 	}
